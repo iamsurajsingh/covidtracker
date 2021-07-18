272a6b1dfbbd7cb031d36779e5578019d50cde17
Suraj <surajthekng7@gmail.com>
2021-07-19 00:02:02 +0530

    Updates

---
+++ precache-manifest.1d48216307e936631a9af040acff2910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77929d4f02137da07c447d5f515325c1",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "29861da4259395ceee1a",
+    "url": "/covidtracker/static/css/2.7d011da2.chunk.css"
+  },
+  {
+    "revision": "ef76543b5fc9ed8326e9",
+    "url": "/covidtracker/static/css/main.199f685f.chunk.css"
+  },
+  {
+    "revision": "29861da4259395ceee1a",
+    "url": "/covidtracker/static/js/2.200a25fa.chunk.js"
+  },
+  {
+    "revision": "74abecdc3638247e00d3cef20bdbaf4e",
+    "url": "/covidtracker/static/js/2.200a25fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef76543b5fc9ed8326e9",
+    "url": "/covidtracker/static/js/main.24eda6a6.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  }
+]);
